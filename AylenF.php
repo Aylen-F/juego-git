d7fcd1eb1badc04de2f4bbf67c01ff6d45633891
Aylen-F <floresguarachiaylen@pedropoveda.edu.bo>
2025-11-27 09:18:28 -0400

    SI

--- AylenF.php
+++ AylenF.php
@@ -6,7 +6,26 @@
     <title>Document</title>
 </head>
 <body>
+    <h1>RESUMEN DE JAVA</h1>
     <h2>Sintaxis de Java</h2>
-    <p>La sintaxis es ,</p>
+    <p>La sintaxis de Java define las reglas para escribir código correctamente: uso obligatorio de llaves {} para bloques, punto y coma ; al final de cada instrucción, sensibilidad a mayúsculas y minúsculas, y una estructura básica que incluye clases, métodos y el método principal main. También establece cómo declarar variables, tipos de datos y operadores.</p>
+<h2>Estructuras repetitivas</h2>
+<p>Son mecanismos que permiten ejecutar un bloque de código varias veces. En Java existen principalmente:
+for: ideal cuando se conoce el número de repeticiones.
+while: se ejecuta mientras la condición sea verdadera.
+do-while: similar a while, pero garantiza al menos una ejecución antes de evaluar la condición.</p>
+
+<h2>Programación Orientada a Objetos (POO): atributos y métodos</h2>
+<p>
+La POO organiza el código en clases que representan objetos del mundo real.
+
+Atributos: variables internas de un objeto que describen sus características.
+
+Métodos: funciones dentro de una clase que definen el comportamiento del objeto.
+El objetivo es encapsular datos y acciones en entidades coherentes.</p>
+
+<h2>Herencia</h2>
+
+<p>Es un principio de la POO que permite crear nuevas clases basadas en clases existentes. Una clase hija (subclase) hereda atributos y métodos de una clase padre (superclase), lo que facilita la reutilización de código y la extensión de funcionalidades sin tener que reescribirlas.</p>
 </body>
 </html>
