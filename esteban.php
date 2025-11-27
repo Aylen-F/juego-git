b5f43e2a2575ce28e3924e4ad61982d2945b9df1
torricolirdmathias <torricolirdmathias@pedropoveda.edu.bo>
2025-11-27 08:45:50 -0400

    devolvindo archivos

---
+++ esteban.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Resumen sobre Java</h1>
+    <p>es un lenguaje de programación utilizado para crear páginas web interactivas y dinámicas.
+    Permitiendo funciones como animaciones, actualizaciones de contenido y formularios interactivos sin necesidad de recargar la página.</p>
+<h2>Sintaxis de JAVA</h2>
+ <p>Define las reglas para escribir código válido(solo se puede declarar por "var","let" o "const"). 
+    Utilizando sentencias que generalmente terminan en punto y coma";" y bloques de código delimitaciones por llaves {}.</p>
+    <h2>Estructuras Repetitivas de JAVA</h2>
+    <p>Permiten ejecutar un bloque de codigo muchas veces. Las principales son ("for", "while" y "do-while")</p>
+    <table>
+        <tr>
+            <th>for</th>
+            <td>Se usa para saber cuantas veces se repitiran</td>
+            <th>while</th>
+            <td>Ejecuta el codigo mientra una condicion sea verdadero</td>
+            <th>do-while</th>
+            <td>Ejecuta el cuerpo del bucle una vez</td>
+        </tr>
+    </table>
+    <h2>POO (Atributos y  Metodos)</h2>
+    <p>Los atributos son los datos que describen las características de un objeto
+    mientras que los métodos son las funciones que definen su comportamiento y las acciones que puede realizar.</p>
+    <h2>Herencia de JAVA</h2>
+    <p>Permite que una clase (la hija o subclase) herede propiedades y métodos de otra clase existente (la padre o superclase). </p>
+    
+</body>
+</html>
